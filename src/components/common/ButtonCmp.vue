7130eb8ca01987cbcbfe372b11e48a353399b89e
ddahyunnam <dhnam1128@gmail.com>
2023-05-04 13:58:43 +0900

    린트 오류 잡는 중
    common / publish > join 작업 중

--- src/components/common/ButtonCmp.vue
+++ src/components/common/ButtonCmp.vue
@@ -2,30 +2,30 @@
     <button
       type="button"
       class="btn"
-      :class="[type, size, icondrection]"      
+      :class="[type, size, icondrection]"
       :disabled="disabled"
       @click="$emit('click')"
       >
       <i
-       v-if="iconname !== null"
-       :class="iconname"
-       >        
+        v-if="iconname !== null"
+        :class="iconname"
+        >
       </i>
-       <slot></slot>
+      <slot></slot>
     </button>
 </template>
 
 <script>
 export default {
-    props: {
+  props: {
+    type: String,
+    disabled: Boolean,
+    size: String,
+    icondrection: String,
+    iconname: {
       type: String,
-      disabled: Boolean,
-      size: String,
-      icondrection: String,
-      iconname: {
-        type: String,
-        default: null
-      }
+      default: null
     }
+  }
 }
 </script>
