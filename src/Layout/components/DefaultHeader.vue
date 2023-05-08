cf64bd31dfd98e8ef4e5155ef863a26395572622
Sunghee <limsh@the-51.com>
2023-05-08 10:32:02 +0900

    이미지 경로 및 레이아웃 일부 추가

--- src/Layout/components/DefaultHeader.vue
+++ src/Layout/components/DefaultHeader.vue
@@ -5,17 +5,23 @@
       <h1><router-link to="/"><span class="blind">RCS Biz Center</span></router-link></h1>
     </div>
     <div class="header__menu">
-      <nav class="nav__inner">
-        <ul>
-          <li v-for="(item, i) in navMenu" :key="i" class="nav-item">
-            <router-link :to="item.to">{{ item.title }}</router-link>
-            <ul class="nav-sub" v-if="item.children">
-              <li v-for="(subItem, i) in item.children" :key="i">
-                <router-link :to="subItem.to">{{ subItem.title }}</router-link>
-              </li>
-            </ul>
-          </li>
+      <nav class="nav">
+        <a role="button" class="nav__button" @click="openNav()">
+          <span class="blind">메뉴</span>
+        </a>
+        <div class="nav__inner">
+          <a role="button" @click="closeNav()" class="nav__button--close">닫기</a>
+          <ul>
+            <li v-for="(item, i) in navMenu" :key="i" class="nav-item">
+              <router-link :to="item.to">{{ item.title }}</router-link>
+              <ul class="nav-sub" v-if="item.children">
+                <li v-for="(subItem, i) in item.children" :key="i">
+                  <router-link :to="subItem.to">{{ subItem.title }}</router-link>
+                </li>
+              </ul>
+            </li>
           </ul>
+        </div>
       </nav>
       <div class="header__util">
         <router-link to="/dashboard" class="link-dashboard">기업 대시보드</router-link>
@@ -43,6 +49,16 @@ export default {
   data() {
     return {
       navMenu
+    }
+  },
+  methods: {
+    openNav() {
+      let el = document.querySelector('.nav')
+      el.classList.add('open')
+    },
+    closeNav() {
+      let el = document.querySelector('.nav')
+      el.classList.remove('open')
     }
   }
 }
