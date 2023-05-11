d3dc12625beb289299d025a30d835a1da538f8e5
Sunghee <limsh@the-51.com>
2023-05-11 14:40:35 +0900

    search table  컴포넌트 수정

--- src/components/common/SearchTable.vue
+++ src/components/common/SearchTable.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="search-table">
-    <Dropdown :options=dropdownOptions v-if="selectable"/>
-    <div class="input">
-      <input type="text" placeholder="검색어를 입력해 주세요.">
+    <div class="ctrl">
+      <Dropdown :options=dropdownOptions v-if="selectable"/>
+      <div class="input">
+        <input type="text" placeholder="검색어를 입력해 주세요.">
+      </div>
+      <div class="input-m">
+        <input type="text" placeholder="검색어 입력">
+      </div>
+      <ButtonCmp
+        type="btn-search"
+      >
+        검색
+      </ButtonCmp>
     </div>
-    <div class="input-m">
-      <input type="text" placeholder="검색어 입력">
-    </div>
-    <ButtonCmp
-      type="search"
-    >
-      검색
-    </ButtonCmp>
     <p class="list-number">
       총 {{ listnumber }}건
     </p>
