96f1986bf2ac0d04487a98d8eb9cfd83aa6f954a
hongsh <hongsh@the-51.com>
2023-05-12 13:57:24 +0900

    우편번호 예시 추가~

--- src/views/join/components/ZipCode.vue
+++ src/views/join/components/ZipCode.vue
@@ -9,18 +9,22 @@
       <div class="modal__content--body">
         <div class="top-search__wrap">
           <div class="search-area">
-            <span class="input"><input type="text" placeholder="도로명 또는 건물명(아파트)을 입력해주세요." /></span>
+            <span class="input">
+              <input type="text" placeholder="도로명 또는 건물명(아파트)을 입력해주세요."
+                @focusin="zipcodeSearch"
+              />
+            </span>
             <ButtonCmp
               type="btn-search"
             >검색</ButtonCmp>
           </div>
           <!-- // validate check error -->
-          <!-- <p class="guide-text error">도로명 또는 건물명(아파트)을 입력해주세요.</p>
+          <!-- <p class="guide-text error" >도로명 또는 건물명(아파트)을 입력해주세요.</p>
           <p class="guide-text error">최소 두 글자 이상 입력해주세요.</p>
           <p class="guide-text error">문자와 숫자를 같이 입력해주세요.</p>
           <p class="guide-text error">특수문자, 숫자만으로는 검색이 어렵습니다.</p> -->
         </div>
-        <div class="search-result__wrap">
+        <div class="search-result__wrap" v-if="postResul">
           <div class="top-ctrl-area">
             <p class="total">총 <strong>1,294</strong>건이 검색되었습니다.​</p>
             <div class="ctrl">
@@ -67,19 +71,22 @@
             </table>
           </div>
         </div>
-        <!-- 최초 출력 <div class="guide-box">
+        <div class="guide-box"
+          v-else
+        >
           <p class="main-text">도로명 주소와 건물번호를 함께 입력하면 빠르게 찾을 수 있습니다.</p>
           <ul>
             <li>&middot; 도로명 + 건물번호 <span class="sm">예) 송파대로 570</span></li>
             <li>&middot; 도로명 <span class="sm">예) 강남대로, 중앙1로, 낙산1길</span></li>
             <li>&middot; 건물명, 아파트명 <span class="sm">예) 반포자이아파트</span></li>
           </ul>
-        </div> -->
+        </div>
       </div>
       <div class="modal__content--footer">
         <div class="button__wrap">
           <ButtonCmp
             type="btn-blue"
+            @click="$emit('closeModal')"
           >닫기</ButtonCmp>
         </div>
       </div>
@@ -126,6 +133,16 @@ import ButtonCmp from '@/components/common/ButtonCmp.vue'
 export default {
   components: {
     ButtonCmp
+  },
+  data() {
+    return {
+      postResul: false
+    }
+  },
+  methods: {
+    zipcodeSearch () {
+      this.postResul = true
+    }
   }
 }
 </script>
